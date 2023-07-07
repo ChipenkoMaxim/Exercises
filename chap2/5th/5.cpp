d98b12359f2920a96aa0f7859db0d7e467d646e1
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-07-07 20:06:48 +0700

    Finish task

--- chap2/5th/5.cpp
+++ chap2/5th/5.cpp
@@ -8,14 +8,92 @@ using namespace std;
 //числа раздёлённые запятой 145,4124,412412,4214,33123 - можно представить в виде int
 
 //3 режима расшифровки
+int upperCaseDecode(int numb) {
+    int processedNumb = numb % 27;
+    if(processedNumb == 0) {
+        return 2;
+    }
+
+    cout << (char)(processedNumb + 64);
+    return 1;
+}
+
+int lowerCaseDecode(int numb) {
+    int processedNumb = numb % 27;
+    if(processedNumb == 0) {
+        return 3;
+    }
+
+    cout << (char)(processedNumb + 96);
+    return 2;
+}
+
+int punctuationDecode(int numb) {
+    int processedNumb = numb % 9;
+    switch (processedNumb)
+    {
+    case 0:
+        return 1;
+    case 1:
+        cout << "!";
+        break;
+    case 2:
+        cout << "?";
+        break;
+    case 3:
+        cout << ",";
+        break;
+    case 4:
+        cout << ".";
+        break;
+    case 5:
+        cout << " ";
+        break;
+    case 6:
+        cout << ";";
+        break;
+    case 7:
+        cout << "\"";
+        break;
+    case 8:
+        cout << "\'";
+        break;
+    default:
+        break;
+    }
+    return 3;
+}
+
 
 int main() {
     //Получить число
-        //Считываем символы до запятой(у последнего числа его может и не быть) или до конца потока
-        //Приводим тип char к int
-        //Если дальшё идёт след число а не запятая, то множим число на десять и прибавляем к нему след цифру
-        //Если далёше идёт запятая то переходим к дешифровке
-    //Раскодировать число
-    //Вывести соответствующий символ
+    char symbol;
+    int number = 0;
+    int mode = 1;
+    while (symbol != 10)
+    {
+        symbol = cin.get();
+        while (symbol != ',' && symbol != 10)
+        {
+        number = number * 10 + (symbol - '0');
+        symbol = cin.get();
+        }
+        //cout << number << endl;
+        switch (mode)
+        {
+        case 1:
+            mode = upperCaseDecode(number);
+            break;
+        case 2:
+            mode = lowerCaseDecode(number);
+            break;
+        case 3:
+            mode = punctuationDecode(number);
+            break;
+        default:
+            break;
+        }
+        number = 0;
+    }
     return 0;
 }
