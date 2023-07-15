077af1ef8a663cbe5bdf01b1fca622ec2fd712bb
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-07-15 19:31:55 +0700

    Solve 3-2 problem

--- chap3/3-2/main.cpp
+++ chap3/3-2/main.cpp
@@ -20,15 +20,54 @@ void fillAgentSales(agent* agents, int AGENT_NUM) {
     }
 }
 
+void printAgent(int* sales, int num) {
+        for(int j = 0; j < num; j++) {
+            cout << sales[j] << " ";
+        }
+        cout << "\n";
+}
+
+
 void printSalesInfo(agent* agents, int AGENT_NUM) {
     for(int i = 0; i < AGENT_NUM; i++) {
         cout << "Agent number " << i + 1 << " sales: ";
-        for(int j = 0; j < agents[i].numOfMonthlySales; j++) {
-            cout << agents[i].monthlySales[j] << " ";
-        }
-        cout << "\n";
+        printAgent(agents[i].monthlySales, agents[i].numOfMonthlySales);
     }
 }
+
+int comparator(const void* first, const void* second) {
+    int* A = (int*)first;
+    int* B = (int*)second;
+    return *A - *B;
+}
+
+int findAverageOfTwo(int first, int second) {
+    return (first + second) / 2;
+}
+
+
+int findMedian(int* sales, int num) {
+    qsort(sales, num, sizeof(int), comparator);
+    int median = 0;
+    if(num % 2 == 0) {
+        median = findAverageOfTwo(sales[num / 2], sales[(num / 2) - 1]);
+    } else {
+        median = sales[num / 2];
+    }
+    return median;
+}
+
+ int findHighestMedian(agent* agents, int AGENT_NUM) {
+    int highestMedian = findMedian(agents[0].monthlySales, agents[0].numOfMonthlySales);
+    for(int i = 1; i < AGENT_NUM; i++) {
+        int salesMedian = findMedian(agents[i].monthlySales, agents[i].numOfMonthlySales);
+        if(salesMedian > highestMedian) {
+            highestMedian = salesMedian;
+        }
+    }
+    return highestMedian;
+ }
+
 
 void releaseMemory(agent** agents, int AGENT_NUM) {
     for(int i = 0; i < AGENT_NUM; i++) {
@@ -48,7 +87,8 @@ int main() {
     agent* agents = new agent[AGENT_NUM];
     fillAgentSales(agents, AGENT_NUM);
     printSalesInfo(agents, AGENT_NUM);
+    int highestMedian = findHighestMedian(agents, AGENT_NUM);
     releaseMemory(&agents, AGENT_NUM);
-
+    cout << "Highest median is equal " << highestMedian;
     return 0;
 }
