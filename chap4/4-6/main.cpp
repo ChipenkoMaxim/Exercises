f178784940fb4ac5bf5ddf4e43d1f8931aaef687
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-07-20 16:46:04 +0700

    Implement append to string using linked list

---
+++ chap4/4-6/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+// Let’s create an implementation for strings that uses a linked list of characters
+// instead of dynamically allocated arrays. So we’ll have a linked list where the
+// data payload is a single char; this will allow strings to grow without having to re-
+// create the entire string. We’ll start by implementing the append and characterAt
+// functions.
+
+using namespace std;
+
+struct charNode {
+    char letter;
+    charNode* nextNode = NULL;
+};
+
+typedef charNode* String;
+
+void initNode(charNode*& node, char newChar) {
+    node = new charNode;
+    node->letter = newChar;
+    node->nextNode = NULL;
+}
+
+void append(String& str, char newChar) {
+    if(str == NULL) {
+        initNode(str, newChar);
+    }
+    else {
+        charNode* currNode = str;
+        while (currNode->nextNode)
+        {
+            currNode = currNode->nextNode;
+        }
+        initNode(currNode->nextNode, newChar);
+    }
+}
+
+
+void inputString(String& str) {
+    char inputChar = cin.get();
+    while (inputChar != 10)
+    {
+        append(str, inputChar);
+        inputChar = cin.get();
+    }
+}
+
+void printString(String str) {
+    while (str)
+    {
+        cout << str->letter;
+        str = str->nextNode;
+    }
+    cout << "\n";
+}
+
+int main() {
+    String str = NULL;
+    inputString(str);
+    printString(str);
+    return 0;
+}
