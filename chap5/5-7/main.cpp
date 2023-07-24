a6d38c71ab4937f7e268e7d421406c6916617619
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-07-24 16:15:06 +0700

    Copmplete addRecord/assignment/delete/copy functions

--- chap5/5-7/main.cpp
+++ chap5/5-7/main.cpp
@@ -16,6 +16,7 @@ class Student {
         };
     public:
         Student();
+        Student(const Student& stud);
         ~Student();
         Student& operator=(const Student& rhs);
         void addRecord(int ID, int grade);
@@ -26,9 +27,93 @@ class Student {
         headRecord _head;
         void deleteRecords(headRecord& head);
         headRecord deepCopy(const headRecord orig);
+        void initRecord(studentRecord*& stud, int id, int grade);
 };
 
 int main() {
-
+    Student stud;
+    for(int i = 0; i < 100; i++) stud.addRecord(1001 + i, i + 1);
+    stud.toString();
+    Student stud1 = stud;
+    stud1.toString();
     return 0;
+}
+
+Student::Student() {
+    _head = NULL;
+}
+
+Student::Student(const Student& stud) {
+    _head = deepCopy(stud._head);
+}
+
+Student::~Student() {
+    deleteRecords(_head);
+}
+
+void Student::deleteRecords(headRecord& head) {
+    while (head)
+    {
+        studentRecord* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+Student& Student::operator=(const Student& rhs) {
+    if(_head != rhs._head) {
+        deleteRecords(_head);
+        _head = deepCopy(rhs._head);
+    }
+    return *this;
+}
+
+Student::headRecord Student::deepCopy(const headRecord orig) {
+    if(orig == NULL) return NULL;
+
+    headRecord newStudList = new studentRecord;
+    newStudList->grade = orig->grade;
+    newStudList->id = orig->id;
+    headRecord newIter = newStudList;
+    headRecord oldIter = orig->next;
+    while (oldIter)
+    {
+        newIter->next = new studentRecord;
+        newIter = newIter->next;
+        newIter->grade = oldIter->grade;
+        newIter->id = oldIter->id;
+        oldIter = oldIter->next;
+    }
+    newIter->next = NULL;
+    return newStudList;
+}
+
+void Student::initRecord(studentRecord*& stud, int id, int grade) {
+    stud = new studentRecord;
+    stud->grade = grade;
+    stud->id = id;
+    stud->next = NULL;
+}
+
+void Student::addRecord(int ID, int grade) {
+    if(_head == NULL) {
+        initRecord(_head, ID, grade);
+        return;
+    }
+
+    studentRecord* iterator = _head;
+    while (iterator->next != NULL)
+    {
+        iterator = iterator->next;
+    }
+    initRecord(iterator->next, ID, grade);
+}
+
+void Student::toString() {
+    studentRecord* iter = _head;
+    while (iter)
+    {
+        cout << "ID = " << iter->id << ", Grade = " << iter->grade << "\n";
+        iter = iter->next;
+    }
 }
