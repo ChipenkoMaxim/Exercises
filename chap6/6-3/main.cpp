f3968fa0054f8d18e8708cf9f5fe124c157572d5
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-07-25 13:44:38 +0700

    Implement calculation of target number occurrences function

---
+++ chap6/6-3/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+// Write a function that is passed an array of integers and a “target” number
+// and that returns the number of occurrences of the target in the array. Solve
+// the problem first using iteration, then recursion.
+
+
+
+using namespace std;
+
+void fillArr(int* arr, int N) {
+    for(int i = 0; i < N; i++) {
+        arr[i] = (rand() % 10) + 1;
+    }
+}
+
+void printArr(int* arr, int N) {
+    for(int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int calcTargetOccurrences(int* arr, int N, int target) {
+    int numOfTarget = 0;
+    for(int i = 0; i < N; i++) {
+        if(arr[i] == target) numOfTarget++;
+    }
+    return numOfTarget;
+}
+
+
+
+int main() {
+    srand(time(NULL));
+    int arr[10];
+    fillArr(arr, 10);
+    printArr(arr, 10);
+    cout << "Num of 5s = " << calcTargetOccurrences(arr, 10, 5) << "\n";
+    
+    return 0;
+}
