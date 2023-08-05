a6f443505b4035e2c15ad7c1e885e94ef6b70f34
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2023-08-06 06:56:32 +0700

    Complete list of words

---
+++ chap8/Chapter_Exercise/hangmang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::ios;
+#include <fstream>
+using std::ifstream;
+#include <string>
+using std::string;
+#include <list>
+using std::list;
+using std::iterator;
+#include <cstring>
+
+
+
+int main() {
+    return 0;
+}
